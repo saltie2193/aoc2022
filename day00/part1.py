61eac115ebf5a105d65f4b64a4ac0b0336582f06
saltie2193 <saltie@protonmail.com>
2022-12-06 23:57:02 +0100

    init[day00]: add template

---
+++ day00/part1.py
@@ -0,0 +1,28 @@
+import os
+
+from aoc import AOC
+from dotenv import load_dotenv
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(HERE, "../.env"))
+API_TOKEN = os.getenv("API_TOKEN")
+
+
+def compute(input_str: str) -> str:
+    return input_str
+
+
+def test() -> None:
+    input_s = ""
+    assert compute(input_s) == ""
+
+
+def main():
+    aoc = AOC(API_TOKEN, HERE)
+    year, day, part = 2022, 0, 0
+
+    aoc.run_part(year, day, part)
+
+
+if __name__ == "__main__":
+    main()
