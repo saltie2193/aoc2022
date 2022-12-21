693f83015e3bac5b74a25f383a66312ab0f9a0c9
saltie2193 <saltie@protonmail.com>
2022-12-21 22:49:17 +0100

    puzzle[day15]: python

---
+++ day15/part1.py
@@ -0,0 +1,121 @@
+from __future__ import annotations
+
+import os
+
+from dotenv import load_dotenv
+from tqdm import tqdm
+
+from aoc import AOC
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(HERE, "../.env"))
+API_TOKEN = os.getenv("API_TOKEN")
+
+
+def manhattan_distance(a: tuple[int, int], b: tuple[int, int]) -> int:
+    return sum(abs(_a - _b) for _a, _b in zip(a, b))
+
+
+def get_row_coverage(
+    row: int, sensor: tuple[int, int, int, int]
+) -> tuple[int, int] | None:
+    (x, y) = sensor[:2]
+    beacon = sensor[2:]
+    dist = manhattan_distance((x, y), beacon)
+    dist_row = abs(x - row)
+
+    if dist_row > dist:
+        return None
+
+    return y - abs(dist - dist_row), y + abs(dist - dist_row)
+
+
+def get_coverage_edge(sensor: tuple[int, int, int, int]) -> set[tuple[int, int]]:
+    (x, y) = sensor[:2]
+    beacon = sensor[2:]
+
+    dist = manhattan_distance((x, y), beacon)
+    print(dist)
+    coverage = set()
+
+    for i in tqdm(range(dist + 1)):
+        j = dist - i
+        coverage.update(
+            [
+                (x + i, y + j),
+                (x + i, y - j),
+                (x - i, y + j),
+                (x - i, y - j),
+            ]
+        )
+    return coverage
+
+
+def parse(input_str: str) -> set[tuple[int, int, int, int]]:
+    sensors: set[tuple[int, int, int, int]] = set()
+
+    for line in input_str.splitlines():
+        v = line.split(" ")
+        sensors.add((int(v[3][2:-1]), int(v[2][2:-1]), int(v[9][2:]), int(v[8][2:-1])))
+
+    return sensors
+
+
+def compute(input_str: str) -> str:
+    sensors = parse(input_str)
+    beacons = {sensor[2:] for sensor in sensors}
+    row = 2000000
+    lower, upper = 0, 0
+
+    for sensor in sensors:
+        res = get_row_coverage(row, sensor)
+        if res is None:
+            continue
+        a, b = res
+        if a < lower:
+            lower = a
+
+        if b > upper:
+            upper = b
+
+    # magic
+    beacons_in_res = [
+        beacon
+        for beacon in beacons
+        if beacon[0] == row and beacon[1] >= lower and beacon[1] <= upper
+    ]
+    print(lower, upper, "->", upper - lower)
+    return str(upper - lower + 1 - len(beacons_in_res))
+
+
+def test() -> None:
+    with open(os.path.join(HERE, "test.txt"), encoding="utf-8") as file:
+        input_s = file.read()
+
+    assert compute(input_s) == "1"
+
+
+def test_manhatten0() -> None:
+    ins = [(0, 0), (0, 1), (1, 0), (1, 1)]
+
+    for i in ins:
+        assert manhattan_distance(i, i) == 0
+
+
+def test_manhatten() -> None:
+    ins = [((0, 0), (0, 1)), ((0, 0), (1, 0)), ((0, 0), (1, 1)), ((1, 1), (5, 4))]
+    outs = [1, 1, 2, 7]
+
+    for i, o in zip(ins, outs):
+        assert manhattan_distance(*i) == o
+
+
+def main():
+    aoc = AOC(API_TOKEN, HERE)
+    year, day, part = 2022, 0, 0
+
+    aoc.run_part(year, day, part, compute)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day15/part2.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+
+import os
+
+import pytest
+from dotenv import load_dotenv
+from tqdm import tqdm
+
+from aoc import AOC
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(HERE, "../.env"))
+API_TOKEN = os.getenv("API_TOKEN")
+
+Sensor = tuple[int, int, int, int]
+Position = tuple[int, int]
+
+
+def manhattan_distance(a: Position, b: Position) -> int:
+    return sum(abs(_a - _b) for _a, _b in zip(a, b))
+
+
+def get_sensor_row_coverage(row: int, sensor: Sensor) -> tuple[int, int] | None:
+    (x, y) = sensor[:2]
+    beacon = sensor[2:]
+    dist = manhattan_distance((x, y), beacon)
+    dist_row = abs(x - row)
+
+    if dist_row > dist:
+        return None
+
+    return y - abs(dist - dist_row), y + abs(dist - dist_row)
+
+
+def merge_coverage_into_row(
+    coverage: tuple[int, int], row: list[tuple[int, int]]
+) -> list[tuple[int, int]]:
+    if len(row) == 0:
+        return [coverage]
+
+    new_row = []
+    i = 0
+    for i, field in enumerate(row):
+        # coverage '>' field
+        if coverage[0] > field[1] + 1:
+            new_row.append(field)
+            continue
+
+        # coverage '<' field
+        if coverage[1] < field[0]:
+            new_row.extend([coverage, *row[i:]])
+            return new_row
+
+        coverage = (min(field[0], coverage[0]), max(field[1], coverage[1]))
+
+    new_row.append(coverage)
+    return new_row
+
+
+def get_row_coverage(row: int, sensors: set[Sensor]) -> list[tuple[int, int]]:
+    coverage = []
+    for sensor in sensors:
+        sensor_coverage = get_sensor_row_coverage(row, sensor)
+        if sensor_coverage is not None:
+            coverage = merge_coverage_into_row(sensor_coverage, coverage)
+    return coverage
+
+
+def get_coverage_edge(sensor: Sensor) -> set[Position]:
+    (x, y) = sensor[:2]
+    beacon = sensor[2:]
+
+    dist = manhattan_distance((x, y), beacon)
+    print(dist)
+    coverage = set()
+
+    for i in tqdm(range(dist + 1)):
+        j = dist - i
+        coverage.update(
+            [
+                (x + i, y + j),
+                (x + i, y - j),
+                (x - i, y + j),
+                (x - i, y - j),
+            ]
+        )
+    return coverage
+
+
+def parse(input_str: str) -> set[Sensor]:
+    sensors: set[Sensor] = set()
+
+    for line in input_str.splitlines():
+        v = line.split(" ")
+        sensors.add((int(v[3][2:-1]), int(v[2][2:-1]), int(v[9][2:]), int(v[8][2:-1])))
+
+    return sensors
+
+
+def compute(input_str: str) -> str:
+    sensors = parse(input_str)
+    l, u = 0, 4000000
+
+    for row_index in tqdm(range(u + 1)):
+        row_coverage = get_row_coverage(row_index, sensors)
+        rc = [(max(x, l), min(y, u)) for (x, y) in row_coverage if x <= u and y >= l]
+        if len(rc) > 1:
+            return str((rc[0][1] + 1) * 4000000 + row_index)
+
+        if len(rc) == 1 and rc[0] != (l, u):
+            return str(max(rc[0][0], l) * min(rc[0][1], u))
+
+    return ""
+
+
+@pytest.mark.skip()
+def test() -> None:
+    with open(os.path.join(HERE, "test.txt"), encoding="utf-8") as file:
+        input_s = file.read()
+
+    assert compute(input_s) == "56000011"
+
+
+def test_manhatten0() -> None:
+    ins = [(0, 0), (0, 1), (1, 0), (1, 1)]
+
+    for i in ins:
+        assert manhattan_distance(i, i) == 0
+
+
+def test_manhatten() -> None:
+    ins = [((0, 0), (0, 1)), ((0, 0), (1, 0)), ((0, 0), (1, 1)), ((1, 1), (5, 4))]
+    outs = [1, 1, 2, 7]
+
+    for i, o in zip(ins, outs):
+        assert manhattan_distance(*i) == o
+
+
+def test_merge_covergae() -> None:
+    row = []
+    steps = [
+        (1, 2),
+        (4, 5),
+        (2, 3),
+        (3, 4),
+        (10, 15),
+        (7, 8),
+        (4, 10),
+    ]
+    outs = [
+        [(1, 2)],
+        [(1, 2), (4, 5)],
+        [(1, 3), (4, 5)],
+        [(1, 5)],
+        [(1, 5), (10, 15)],
+        [(1, 5), (7, 8), (10, 15)],
+        [(1, 15)],
+    ]
+
+    for i, step in enumerate(steps):
+        row = merge_coverage_into_row(step, row)
+        print(step, row)
+        assert row == outs[i]
+
+
+def main():
+    aoc = AOC(API_TOKEN, HERE)
+    year, day, part = 2022, 0, 0
+
+    aoc.run_part(year, day, part, compute)
+
+
+if __name__ == "__main__":
+    main()
